7f445455a183f6215782f2ac1d19e7a498f7a909
varsha <varshaprajapati.15004@gmail.com>
2022-08-13 21:19:20 +0530

    Updates

---
+++ precache-manifest.ed94242567f0d58093e7e151909fc747.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db3ad6553b7cee4b1fc4ce16194795a",
+    "url": "/ToDoList-React/index.html"
+  },
+  {
+    "revision": "2db41e42c81d263727d7",
+    "url": "/ToDoList-React/static/js/2.d73984dc.chunk.js"
+  },
+  {
+    "revision": "9b3780dbc02eea700768",
+    "url": "/ToDoList-React/static/js/main.f6538dd1.chunk.js"
+  },
+  {
+    "revision": "0a8c88668517d628d31e",
+    "url": "/ToDoList-React/static/js/runtime-main.a5ce8233.js"
+  }
+]);
